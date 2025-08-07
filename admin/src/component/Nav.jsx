a691bc53fd307ff0cc4a3255d1530248aa92e758
Nikunj Usadadiya <nikunjusadadiya19@gmail.com>
2025-08-07 11:51:14 +0530

    Update Nav.jsx

--- admin/src/component/Nav.jsx
+++ admin/src/component/Nav.jsx
@@ -29,7 +29,7 @@ let {getAdmin}=useContext(adminDataContext)
     <div className='w-full h-[60px] bg-white z-50 fixed  top-0 flex items-center  justify-between px-[30px] overflow-x-hidden shadow-md shadow-black'>
       <div className='w-[30%] flex items-center justify-start gap-[10px] cursor-pointer'>
       {/* <img src='' className='w-[30px]'/> */}
-      <h1 className='md:text-[25px] text-[23px] p-[35px] md:p-[0px] text-black font-sans '>Mobile&nbsp;Zone</h1>
+      <h1 className='md:text-[25px] text-[23px] p-[35px] md:p-[0px] text-black font-sans '>Speak&nbsp;Deals</h1>
 
       </div>
       <button onClick={logout} className='text-[15px]  border-[black] cursor-pointer bg-white py-[10px] px-[20px] rounded-2xl text-black'>Logout
